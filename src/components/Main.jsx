5fd8e1f419cf65bf8803f2ec001914225f29d755
V, Dhanush <Dhanush.V@cerner.com>
2023-01-07 17:23:34 +0530

    Making API call async

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,11 +11,14 @@ export default function Main(){
 
     const [allMeme,setAllMeme] = React.useState()
 
-    React.useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(data => setAllMeme(data.data.memes))
-    },[])
+    React.useEffect(() =>{
+        async function getMemes(){
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMeme(data.data.memes)
+        }
+        getMemes()
+    })
 
     function handleChange(event){
         const {name, value} = event.target
